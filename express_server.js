628bec0bd57ebbb87104258746aa2c60cd132158
Anastasia <z.anastasia11@gmail.com>
2023-03-31 13:25:48 -0700

    define the route that will match this POST request and handle it

--- express_server.js
+++ express_server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+//translates, or parses the body from Buffer to human readable data;
+//The body-parser library will convert the request body from a Buffer into string that we can read. It will then add the data to the req(request) object under the key body.
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,6 +22,11 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }; //send variables inside an object
   //console.log(templateVars);
   res.render("urls_index", templateVars); //sending variables to an EJS template urls_index
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
