8ee72b9d02b4eb6dbebc67bb64c2ef3b61d14858
Anastasia <z.anastasia11@gmail.com>
2023-03-31 11:59:29 -0700

    add handker to Render information about a single URL

--- express_server.js
+++ express_server.js
@@ -16,18 +16,24 @@ app.get("/", (req, res) => {
 //Why in an object?????
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }; //send variables inside an object
+  //console.log(templateVars);
   res.render("urls_index", templateVars); //sending variables to an EJS template urls_index
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars); // 2nd
 });
-
-
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+/*
+Design backend instagram
+ /posts/:id => customer /post/
+ 
+ /followers/:id
+ /following
+ /reels
+*/
