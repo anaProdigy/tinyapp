dff4c102490528a30f14be7b3a4c13dcd7a70862
Anastasia <z.anastasia11@gmail.com>
2023-03-31 15:40:50 -0700

    add the code so that requests to the endpoint /u/:id will redirect to its longURL

--- express_server.js
+++ express_server.js
@@ -54,7 +54,7 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.get("/u/:id", (req, res) => {
-  // const longURL = ...
+  const longURL = urlDatabase[req.params.id]
   res.redirect(longURL);
 });
 
