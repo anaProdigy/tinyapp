7844aef9e38acae518fba4556078877c5f90fc36
Anastasia <z.anastasia11@gmail.com>
2023-03-31 13:01:08 -0700

    add get route for urls_new template

--- express_server.js
+++ express_server.js
@@ -20,6 +20,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars); //sending variables to an EJS template urls_index
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars); // 2nd
